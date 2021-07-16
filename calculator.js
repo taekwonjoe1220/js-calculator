4594778d5e55bec7a861c14e5cb7157c99b63591
Joe Forster <jforster.advanced@gmail.com>
2021-07-16 07:02:39 -0600

    added display update listener for numpad, need to work on limiting to 1 decimal

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+const display = document.querySelector(".display");
 function add(a, b) {
   return a + b;
 }
@@ -14,3 +15,21 @@ function divide(a, b) {
 function operate(a, b, operator) {
   return operator(a, b);
 }
+
+const numpad = document.querySelectorAll(".num");
+
+// event listener to update display with numbers. Need to update this to account for using a limit of one decimal
+numpad.forEach((num) =>
+  num.addEventListener("click", () => {
+    if (display.textContent === "0") {
+      display.textContent = num.textContent;
+    } else if (
+      display.textContent.length === 10 &&
+      display.textContent.includes(".")
+    ) {
+      return;
+    } else {
+      display.textContent += num.textContent;
+    }
+  })
+);
