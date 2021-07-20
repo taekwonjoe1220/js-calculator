ff66b48bd41b4a64f9c32698b9e975e8d35e5404
Joe Forster <jforster.advanced@gmail.com>
2021-07-20 14:40:01 -0600

    added decimal functionality

--- calculator.js
+++ calculator.js
@@ -1,4 +1,31 @@
 const display = document.querySelector(".display");
+const numpad = document.querySelectorAll(".num");
+const decimal = document.querySelector(".decimal");
+let value = 0;
+
+// event listener to update display with numbers. Need to update this to account for using a limit of one decimal
+numpad.forEach((num) =>
+  num.addEventListener("click", () => {
+    if (num.classList.contains("decimal")) {
+      return;
+    }
+    if (display.textContent === "0") {
+      display.textContent = num.textContent;
+    } else if (display.textContent.length >= 9) {
+      // do nothing to display
+    } else {
+      display.textContent += num.textContent;
+    }
+    value = Number(display.textContent);
+  })
+);
+decimal.addEventListener("click", () => {
+  if (!display.textContent.includes(".")) {
+    display.textContent += ".";
+  }
+});
+
+// Operation functions
 function add(a, b) {
   return a + b;
 }
@@ -9,27 +36,11 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if (b == 0) {
+    return `error (div. by zero)`;
+  }
   return a / b;
 }
-
 function operate(a, b, operator) {
   return operator(a, b);
 }
-
-const numpad = document.querySelectorAll(".num");
-
-// event listener to update display with numbers. Need to update this to account for using a limit of one decimal
-numpad.forEach((num) =>
-  num.addEventListener("click", () => {
-    if (display.textContent === "0") {
-      display.textContent = num.textContent;
-    } else if (
-      display.textContent.length === 10 &&
-      display.textContent.includes(".")
-    ) {
-      return;
-    } else {
-      display.textContent += num.textContent;
-    }
-  })
-);
