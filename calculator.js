ea00e99c2262f94d59ddb389339ce9d5bbcc8c67
Joe Forster <jforster.advanced@gmail.com>
2021-07-29 22:19:16 -0600

    added operation functionality, +/- button works, equals button works, overally functionality close to done - only minor adjustments needed :)

--- calculator.js
+++ calculator.js
@@ -7,6 +7,10 @@ const multiply = document.querySelector(".multiply");
 const subtract = document.querySelector(".subtract");
 const add = document.querySelector(".add");
 const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const alternateSign = document.querySelector(".plus-minus");
+
+let hasTyped = false;
 
 function getValue() {
   return Number(display.textContent);
@@ -14,14 +18,16 @@ function getValue() {
 
 // initial state
 let value1 = 0,
-  value2 = 0;
+  value2 = 0,
+  result;
 let operation = null;
 
 // event listener to update display with numbers. Need to update this to account for using a limit of one decimal
 numpad.forEach((num) =>
   num.addEventListener("click", () => {
-    if (display.textContent === "0") {
+    if (!hasTyped) {
       display.textContent = num.textContent;
+      hasTyped = true;
     } else if (display.textContent.length < 9) {
       display.textContent += num.textContent;
     }
@@ -37,7 +43,7 @@ decimal.addEventListener("click", () => {
 
 // Operation functions
 function addition(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 function subtraction(a, b) {
   return a - b;
@@ -52,32 +58,99 @@ function division(a, b) {
   return a / b;
 }
 function operate(a, b, operator) {
-  return operator(a, b);
+  switch (operator) {
+    case "addition":
+      result = addition(a, b);
+      break;
+    case "subtraction":
+      result = subtraction(a, b);
+      break;
+    case "multiplication":
+      result = multiplication(a, b);
+      break;
+    case "division":
+      result = division(a, b);
+      break;
+  }
 }
 
-// click events for operators
+// click events for operators & +/-
 
-// divide.addEventListener("click", () => {
-//   operation = "division";
-// });
-
-// multiply.addEventListener("click", () => {
-//   operation = "multiplication";
-// });
-
+// addition
 add.addEventListener("click", () => {
-  // operation = "test";
   if (operation == null) {
-    operation = "addition";
     value1 = getValue();
   } else {
     value2 = getValue();
+    operate(value1, value2, operation);
+    display.textContent = result;
+    value1 = display.textContent;
   }
-
-  //   // console.log(operate(value1, value2, operator));
-  // }
+  operation = "addition";
+  hasTyped = false;
 });
-// subtract.addEventListener("click", () => {
-//   operation = "subtraction";
-// });
-// Inside event listener, store current display #, store operator name (check if operator active or null)
+// subtraction
+subtract.addEventListener("click", () => {
+  if (operation == null) {
+    value1 = getValue();
+  } else {
+    value2 = getValue();
+    operate(value1, value2, operation);
+    display.textContent = result;
+    value1 = display.textContent;
+  }
+  operation = "subtraction";
+  hasTyped = false;
+});
+// multiplication
+multiply.addEventListener("click", () => {
+  if (operation == null) {
+    value1 = getValue();
+  } else {
+    value2 = getValue();
+    operate(value1, value2, operation);
+    display.textContent = result;
+    value1 = display.textContent;
+  }
+  operation = "multiplication";
+  hasTyped = false;
+});
+// division
+divide.addEventListener("click", () => {
+  if (operation == null) {
+    value1 = getValue();
+  } else {
+    value2 = getValue();
+    operate(value1, value2, operation);
+    display.textContent = result;
+    value1 = display.textContent;
+  }
+  operation = "division";
+  hasTyped = false;
+});
+// equals
+equals.addEventListener("click", () => {
+  if (operation == null) {
+    // do nothing
+  } else {
+    value2 = getValue();
+    operate(value1, value2, operation);
+    display.textContent = result;
+    value1 = display.textContent;
+  }
+});
+
+// +/- toggle
+alternateSign.addEventListener("click", () => {
+  display.textContent = Number(display.textContent) * -1;
+});
+
+// clear function
+clear.addEventListener("click", () => {
+  value1 = 0;
+  value2 = 0;
+  result = 0;
+  operation = null;
+  display.textContent = "0";
+  hasTyped = false;
+});
